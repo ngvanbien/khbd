a2f5cbf6e3311cdd29c41345be240ac38348d09d
ngvanbien <vanbien@gmail.com>
2026-04-01 13:39:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react' // hoặc vue
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
